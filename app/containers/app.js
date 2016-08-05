3f22a6a653be93ca889d1d90a7fd92099f97dacc
gongdadong <411298820@qq.com>
2016-08-05 16:20:05 +0800

    Add react-redux depenence

--- app/containers/app.js
+++ app/containers/app.js
@@ -15,7 +15,7 @@ import * as API from './api';
 import Home from  './containers/home';
 import TabNavigator from 'react-native-tab-navigator';
 
-export default class prestaApp extends Component {
+class prestaApp extends Component {
     constructor(props) {
         super(props);
         this.state = {selectedTab: 'home'};
@@ -24,8 +24,7 @@ export default class prestaApp extends Component {
     render() {
         return (
             <TabNavigator
-                tabBarStyle={styles.tab}
-                sceneStyle={{ paddingBottom: 0 }}>
+                tabBarStyle={styles.tab}>
                 <TabNavigator.Item
                     selected={this.state.selectedTab === 'home'}
                     title="Home"
@@ -44,24 +43,6 @@ export default class prestaApp extends Component {
                     onPress={() => this.setState({ selectedTab: 'tab2' })}>
                     <Text>123</Text>
                 </TabNavigator.Item>
-                <TabNavigator.Item
-                    selected={this.state.selectedTab === 'tab3'}
-                    title="tab3"
-                    titleStyle={styles.tabTitle}
-                    renderIcon={() => <Image style={styles.tabIcon} source={{uri:'https://maxcdn.icons8.com/Share/icon/Messaging//star1600.png'}} />}
-                    renderSelectedIcon={() => <Image style={styles.tabIcon} source={{uri:'https://maxcdn.icons8.com/Share/icon/Messaging//star1600.png'}} />}
-                    onPress={() => this.setState({ selectedTab: 'tab3' })}>
-                    <Text>123</Text>
-                </TabNavigator.Item>
-                <TabNavigator.Item
-                    selected={this.state.selectedTab === 'tab4'}
-                    title="tab4"
-                    titleStyle={styles.tabTitle}
-                    renderIcon={() => <Image style={styles.tabIcon} source={{uri:'https://maxcdn.icons8.com/Share/icon/Messaging//star1600.png'}} />}
-                    renderSelectedIcon={() => <Image style={styles.tabIcon} source={{uri:'https://maxcdn.icons8.com/Share/icon/Messaging//star1600.png'}} />}
-                    onPress={() => this.setState({ selectedTab: 'tab4' })}>
-                    <Text>123</Text>
-                </TabNavigator.Item>
             </TabNavigator>
         );
     }
@@ -69,8 +50,6 @@ export default class prestaApp extends Component {
 
 const styles = StyleSheet.create({
     tab:{
-        height:0,
-        overflow:'hidden'
     },
     tabIcon: {
         width: 30,
